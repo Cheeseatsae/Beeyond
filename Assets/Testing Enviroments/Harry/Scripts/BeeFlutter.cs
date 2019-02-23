6f9082cc4c6516238084a3eafec35d3afc40d960
HarrisonLorraway <1011422@student.sae.edu.au>
2019-02-23 16:58:54 +1100

    Semi-working/broken bee movement
    Bee flutter with sin wave works well, issues with using force to move. in progress

---
+++ Assets/Testing Enviroments/Harry/Scripts/BeeFlutter.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+namespace Harry
+{
+    public class BeeFlutter : MonoBehaviour
+    {
+        [Range(0,1)]
+        public float speed;
+        [Range(0,1)]
+        public float amplitude;
+        [Range(0,2)]
+        public float distance;
+        [Range(0,2)]
+        public float amount;
+
+        public GameObject parent;
+        
+        private float _sinTime;
+
+        public float floatingRange;
+
+        private Rigidbody _myBody;
+        private float _yValue;
+        private bool _zeroed = true;
+
+        private void Awake()
+        {
+            _myBody = GetComponent<Rigidbody>();
+            _sinTime = Random.Range(0, 1f);
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            // sin(_Time.y * _Speed + v.vertex.y * _Amplitude) * _Distance * _Amount;
+    
+            _sinTime += Time.deltaTime;
+            
+            _yValue = Mathf.Sin(_sinTime * speed + transform.position.y * amplitude) * distance * amount;
+    
+                if (Vector3.Distance(transform.position, parent.transform.position) > floatingRange)     
+                    _zeroed = false;
+                
+                if (_zeroed) 
+                    _myBody.velocity = new Vector3(0, _yValue, 0);
+                else
+                {
+                    _myBody.velocity = new Vector3(0, parent.transform.position.y - transform.position.y, 0) ;
+    
+                    if (Vector3.Distance(transform.position, parent.transform.position) < 0.02f)
+                    {
+                        _sinTime = 0;
+                        _zeroed = true;
+                    } 
+                        
+                }
+    
+            }
+        
+    }
+
+}
+
---
+++ Assets/Testing Enviroments/Harry/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Harry;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+namespace Harry
+{
+    
+    public class PlayerController : MonoBehaviour
+    {
+        private GameObject _beeChild;
+        private Rigidbody _beeBody;
+
+        public string inputHorizontal, inputVertical;
+        public float speedMult = 0.05f;
+        public float maxSpeed;
+
+        private float _inputHorizontalValue { get { return Input.GetAxis(inputHorizontal); } }
+        private float _inputVerticalValue { get { return Input.GetAxis(inputVertical); } } 
+
+        private void Awake()
+        {
+            _beeChild = GetComponentInChildren<BeeFlutter>().gameObject;
+            _beeBody = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+            transform.position = new Vector3(_beeChild.transform.position.x, transform.position.y, _beeChild.transform.position.z);
+
+        
+            Debug.Log("speed is " + _beeBody.velocity);
+            _beeBody.velocity -= new Vector3(_inputHorizontalValue, _inputVerticalValue + _beeBody.velocity.y, 0) * speedMult;
+
+            _beeBody.velocity = Vector3.ClampMagnitude(_beeBody.velocity, maxSpeed);
+
+        }
+    }
+
+}
+
