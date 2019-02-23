36bc42133a04defbfdc4f1ba1eaedb362214d0da
HarrisonLorraway <1011422@student.sae.edu.au>
2019-02-23 19:42:18 +1100

    Bee movement progress, still slightly broken

--- Assets/Testing Enviroments/Harry/Scripts/PlayerController.cs
+++ Assets/Testing Enviroments/Harry/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using Harry;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 using Vector3 = UnityEngine.Vector3;
 
 namespace Harry
@@ -17,9 +18,26 @@ namespace Harry
         public float speedMult = 0.05f;
         public float maxSpeed;
 
-        private float _inputHorizontalValue { get { return Input.GetAxis(inputHorizontal); } }
-        private float _inputVerticalValue { get { return Input.GetAxis(inputVertical); } } 
-
+        private int _inputHorizontalValue 
+        { 
+            get
+            {
+                if (Input.GetAxisRaw(inputHorizontal) < -0.1f) return -1;
+                else if (Input.GetAxisRaw(inputHorizontal) > -0.1f && Input.GetAxisRaw(inputHorizontal) < 0.1f) return 0;
+                else return 1;
+            } 
+        }
+        
+        private int _inputVerticalValue 
+        { 
+            get
+            {
+                if (Input.GetAxisRaw(inputVertical) < -0.1f) return -1;
+                else if (Input.GetAxisRaw(inputVertical) > -0.1f && Input.GetAxisRaw(inputVertical) < 0.1f) return 0;
+                else return 1;
+            } 
+        }
+        
         private void Awake()
         {
             _beeChild = GetComponentInChildren<BeeFlutter>().gameObject;
@@ -31,13 +49,23 @@ namespace Harry
         {
             
             transform.position = new Vector3(_beeChild.transform.position.x, transform.position.y, _beeChild.transform.position.z);
+            
+            
+            //Debug.Log("speed is " + _beeBody.velocity);
+            Debug.Log("X is " + _inputHorizontalValue + "          Y is " + _inputVerticalValue);
+            
+            _beeBody.velocity += new Vector3(_inputHorizontalValue * 2, _inputVerticalValue + _beeBody.velocity.y, 0) * speedMult;
 
-        
-            Debug.Log("speed is " + _beeBody.velocity);
-            _beeBody.velocity -= new Vector3(_inputHorizontalValue, _inputVerticalValue + _beeBody.velocity.y, 0) * speedMult;
+            if (_inputHorizontalValue == 0 && _inputVerticalValue == 0)
+            {
+                _beeBody.velocity = Vector3.Lerp(_beeBody.velocity, Vector3.zero, 0.1f);
+            }
 
-            _beeBody.velocity = Vector3.ClampMagnitude(_beeBody.velocity, maxSpeed);
+        }
 
+        private void FixedUpdate()
+        {
+            _beeBody.velocity = new Vector3(Mathf.Clamp(_beeBody.velocity.x, -maxSpeed, maxSpeed), Mathf.Clamp(_beeBody.velocity.y, -maxSpeed, maxSpeed), 0);
         }
     }
 
