e8b7c06a0f1992d8d08bd7df407a03dc5457a94c
Harry <1011422@student.sae.edu.au>
2019-03-13 18:22:57 +1100

    BeeController is now a parent, AI less broken and new scripts made

---
+++ Assets/Testing Enviroments/Harry/Scripts/PlayerBeeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Harry;
+using UnityEngine;
+
+namespace Harry
+{
+    
+    public class PlayerBeeController : BeeController
+    {
+   
+        // Update is called once per frame
+        public override void FixedUpdate()
+        {
+        
+            // HACK will need to be redone later 
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                switch (myState)
+                {
+                    case BeeState.Stopped:
+                        myState = BeeState.Pollenated;
+                        currentFlower.Harvest();
+
+                        break;
+
+                    case BeeState.Pollenated:
+                        myState = BeeState.Moving;
+
+                        break;  
+                }
+               
+            }
+
+            // if we're stopped do nothing
+            if (myState == BeeState.Stopped) return;
+        
+            base.FixedUpdate();
+        
+        }
+    }
+    
+}
+
---
+++ Assets/Testing Enviroments/Russell/AIBeeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Harry
+{
+    
+    public class AIBeeController : BeeController
+    {
+        
+        public float minDist;
+        public float maxDist;
+        public Color rayColor = Color.green;
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            
+            RayCastDistanceCheck();
+        }
+
+        public void RayCastDistanceCheck()
+        {
+            foreach (GameObject bees in _whatsAround.WhosAround)
+            {
+                float distance;
+                Debug.Log(bees + "now raycast");
+                //rayColor = Color.red;
+                
+                if (Physics.Raycast(_myBody.transform.position, (_myBody.transform.position - bees.transform.position), _whatsAround.raduisOfSphere))
+                {
+                    Debug.DrawRay(transform.position, (bees.transform.position - _myBody.transform.position), rayColor);
+                    distance = Vector3.Distance(transform.position, bees.transform.position);
+                    if(distance <= minDist )
+                    {
+                        Debug.Log(bees + "is to close");
+                        Debug.DrawRay(transform.position, (bees.transform.position - _myBody.transform.position), Color.red);
+                        //move character away from close object
+                    }
+                    else if (distance >= maxDist)
+                    {
+                        Debug.Log("I need to move closer to" + bees);
+                        Debug.DrawRay(transform.position, (bees.transform.position - _myBody.transform.position), Color.blue);
+                        //move closer to object
+                    }
+                }
+            }
+        }
+        
+    }
+    
+}
+
+
