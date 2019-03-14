68fee89b565612cb14f272d2796063f2b12f5960
Jing <1013263@student.sae.edu.au>
2019-03-14 16:02:38 +1100

    Testing analytics

---
+++ Assets/Testing Enviroments/Jing/AnalyticsEvents.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class AnalyticsEvents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TriggerIsValid();
+    }
+
+
+    //Debug TriggerBox
+    void TriggerIsValid()
+    {
+        //when bee enter the collider
+        //Debug.Log("Trigger is working");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
