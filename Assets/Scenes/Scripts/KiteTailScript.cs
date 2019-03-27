cfe466c79bb25747514886d3348f28002c194d23
Roo <roo.kooistra@gmail.com>
2019-03-27 22:47:18 +1100

    More Level build
    flower positions, camera boundaries, and various cool shit

---
+++ Assets/Scenes/Scripts/KiteTailScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KiteTailScript : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    [Range(0f,2f)] public float windStrength = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(-(Roo.WindScript.windSpeed * windStrength), 0, 0);
+    }
+}
