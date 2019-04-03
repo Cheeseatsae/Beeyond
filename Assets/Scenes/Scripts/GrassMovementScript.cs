9761681d755432b005625a84787feed2e42746f1
Roo <roo.kooistra@gmail.com>
2019-04-03 18:38:53 +1100

    Implemented work around grass concept
    Implemented grass concept for close up blades

---
+++ Assets/Scenes/Scripts/GrassMovementScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassMovementScript : MonoBehaviour
+{
+    private Cloth grassCloth;
+    [Range(0f,2f)] public float windMultiplierLowerRange = 0.5f;
+    [Range(0f, 2f)] public float windMultiplierUpperRange = 1f;
+
+    private float _windMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grassCloth = GetComponent<Cloth>();
+
+        //set random attributes
+        grassCloth.bendingStiffness = Random.Range(.5f, 1f);
+        grassCloth.friction = Random.Range(.2f, .3f);
+        grassCloth.damping = Random.Range(.05f, .15f);
+
+        _windMultiplier = Random.Range(windMultiplierLowerRange, windMultiplierUpperRange);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        grassCloth.externalAcceleration = new Vector3(-(Roo.WindScript.windSpeed * _windMultiplier), 1f, 0f);
+    }
+}
