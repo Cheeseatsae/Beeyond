95b488a4fa9b218600ef59a0949f2e5354d49962
Roo <roo.kooistra@gmail.com>
2019-04-20 00:03:20 +1000

    Building Reward Scene

--- Assets/Scenes/Scripts/ButterflyFlutterScript.cs
+++ Assets/Scenes/Scripts/ButterflyFlutterScript.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ButterflyFlutterScript : MonoBehaviour
 {
-    [Range(0.5f, 5f)] public float minAnimationSpeed = 1.75f;
-    [Range(0.5f, 5f)] public float maxAnimationSpeed = 2.25f;
+    [Range(0.1f, 5f)] public float minAnimationSpeed = 1.75f;
+    [Range(0.1f, 5f)] public float maxAnimationSpeed = 2.25f;
 
     // Start is called before the first frame update
     
