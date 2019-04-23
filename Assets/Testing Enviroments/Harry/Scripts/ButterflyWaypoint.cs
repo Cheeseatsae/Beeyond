45834aff9ef82021f25bda1c5a6136e1a1adb399
HarrisonLorraway <1011422@student.sae.edu.au>
2019-04-23 21:31:39 +1000

    I'm gonna make your clouds fluorescent purple - also butterfly fix

--- Assets/Testing Enviroments/Harry/Scripts/ButterflyWaypoint.cs
+++ Assets/Testing Enviroments/Harry/Scripts/ButterflyWaypoint.cs
@@ -25,6 +25,11 @@ namespace Harry
             Gizmos.color = new Color(1,0.92f,0.016f, transparency);
             Gizmos.DrawSphere(transform.position, radius);
         }
+
+        private void OnDestroy()
+        {
+            waypoints.Clear();
+        }
     }
     
 }
