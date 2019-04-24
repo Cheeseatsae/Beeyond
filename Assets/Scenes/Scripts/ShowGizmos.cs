7cf258ede2e363d7fc955edb5c9b226eb801541f
Roo <roo.kooistra@gmail.com>
2019-04-24 15:17:46 +1000

    Post processing volumes done

---
+++ Assets/Scenes/Scripts/ShowGizmos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowGizmos : MonoBehaviour
+{
+    [Range(0, 1)] public float redChannel;
+    [Range(0, 1)] public float greenChannel;
+    [Range(0, 1)] public float blueChannel;
+    [Range(0, 1)] public float transparency;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(redChannel,greenChannel,blueChannel,1);
+        Gizmos.DrawWireCube(transform.position, GetComponent<Collider>().bounds.size);
+        Gizmos.color = new Color(redChannel, greenChannel, blueChannel, transparency);
+        Gizmos.DrawCube(transform.position, GetComponent<Collider>().bounds.size);
+    }
+}
