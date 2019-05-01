a109477dd4491587c00791ecf367a1d075940c2d
Roo <roo.kooistra@gmail.com>
2019-05-01 15:40:29 +1000

    script for swing collision

---
+++ Assets/Scenes/Scripts/SwingSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingSound : MonoBehaviour
+{
+    private bool _hasPlayed = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "BlockerBottom")
+        {
+            if (!_hasPlayed)
+            {
+                _hasPlayed = true;
+                AudioManagerScript.Playsound("HeavyObjectFallyingGrass03");
+            }
+        }
+    }
+}
